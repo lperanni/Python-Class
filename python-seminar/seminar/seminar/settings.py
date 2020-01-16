475af81ef681cc375743a5634393d2cfef40fb0a
lperanni <lucianoperanni95@gmail.com>
2020-01-16 02:48:55 +0100

    Added MySql connection to XAMPP mysql instance

--- python-seminar/seminar/seminar/settings.py
+++ python-seminar/seminar/seminar/settings.py
@@ -73,13 +73,26 @@ WSGI_APPLICATION = 'seminar.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'djangoproject',
+        'USER': 'root',
+        'PASSWORD': "",
+        'HOST': "127.0.0.1",
+        'PORT': "",
+        'OPTIONS': {
+                'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
